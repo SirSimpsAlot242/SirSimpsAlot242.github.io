4e8cf2e60e72e243f5540937c4ae90eba79d5530
Cartney Michael <829810@stma.k12.mn.us>
2022-04-04 12:47:14 -0500

    coloer

---
+++ main.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+function strawberry() {
+	document.getElementById("Hello").innerHTML = "Strawberry";
+	document.getElementById("Hello").style.backgroundColor = "Red";
+}
+function ooga() {
+	document.getElementById("Hello").innerHTML = "Ooga";
+	document.getElementById("Hello").style.backgroundColor = "Blue";
+}
+function booga() {
+	document.getElementById("Hello").innerHTML = "Booga";
+	document.getElementById("Hello").style.backgroundColor = "Green";
+}
+function legoStarWars() {
+	document.getElementById("Hello").innerHTML = "Lego Star Wars";
+	document.getElementById("Hello").style.backgroundColor = "Yellow";
+}
+function masonIsDogWater() {
+	document.getElementById("Hello").innerHTML = "Mason is dog water";
+	document.getElementById("Hello").style.backgroundColor = "Purple";
+}
+function grey() {
+	document.getElementById("Hello").innerHTML = "Grey";
+	document.getElementById("Hello").style.backgroundColor = "Grey";
+}
+function never() {
+	document.getElementById("Hello").innerHTML = "Never";
+	document.getElementById("Hello").style.backgroundColor = "DrakGrey";
+}
+function gonna() {
+	document.getElementById("Hello").innerHTML = "Gonna";
+	document.getElementById("Hello").style.backgroundColor = "Brown";
+}
+function give() {
+	document.getElementById("Hello").innerHTML = "Give";
+	document.getElementById("Hello").style.backgroundColor = "Steel";
+}
+function you() {
+	document.getElementById("Hello").innerHTML = "You";
+	document.getElementById("Hello").style.backgroundColor = "Light Blue";
+}
+function up() {
+	document.getElementById("Hello").innerHTML = "Up";
+	document.getElementById("Hello").style.backgroundColor = "White";
+}
+
+
+
+
+
+
+document.getElementById("1button").addEventListener("click", strawberry);
+document.getElementById("2button").addEventListener("click", ooga);
+document.getElementById("3button").addEventListener("click", booga);
+document.getElementById("4button").addEventListener("click", legoStarWars);
+document.getElementById("5button").addEventListener("click", masonIsDogWater);
+document.getElementById("6button").addEventListener("click", grey);
+document.getElementById("7button").addEventListener("click", never);
+document.getElementById("8button").addEventListener("click", gonna);
+document.getElementById("9button").addEventListener("click", give);
+document.getElementById("10button").addEventListener("click", you);
+document.getElementById("11button").addEventListener("click", up);
