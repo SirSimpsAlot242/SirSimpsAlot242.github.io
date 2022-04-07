8288540062f17cb2708df8c5924c19bc3218ae53
Cartney Michael <829810@stma.k12.mn.us>
2022-04-07 12:28:43 -0500

    pass

--- secret.js
+++ secret.js
@@ -1,1 +1,15 @@
-document.getElementById("password").value
+function password() {
+	if (document.getElementById("password").value == "BonBon"){
+	document.getElementById("password").style.backgroundColor = "Green"
+		document.getElementById("password").value = "Acess Granted"
+	}
+	else if (document.getElementById("password").value != "BonBon"){
+		document.getElementById("password").style.backgroundColor = "Red"
+		document.getElementById("password").value = "Acess Denied"
+	}
+	
+}
+
+
+
+document.getElementById("butt").addEventListener("click",password );
